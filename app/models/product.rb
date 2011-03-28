7a572704e522a556efc75d3a7f04f9e250b102e2
Jack Xiang <jackx@reallyenglish.com>
2011-03-28 13:28:35 +0800

    Prettier listings

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+  
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
   validates :image_url, :format => {:with => %r{\.(gif|png|jpg)$}i,
                                     :message => 'must be a URL for GIF, PNG or JPG image.'}
+  validates :title, :length => {:minimum => 10}
 end
