832bb718b56839f8440d23d7b4e437692f98c01d
Jack Xiang <jackx@reallyenglish.com>
2011-03-28 16:06:03 +0800

    Adding a Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'title'
+  has_many :line_items
+  
+  before_destroy :ensure_not_referenced_by_any_line_item
   
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
@@ -7,4 +10,16 @@ class Product < ActiveRecord::Base
   validates :image_url, :format => {:with => %r{\.(gif|png|jpg)$}i,
                                     :message => 'must be a URL for GIF, PNG or JPG image.'}
   validates :title, :length => {:minimum => 10}
+  
+  
+  private
+    #ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.count.zero?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
